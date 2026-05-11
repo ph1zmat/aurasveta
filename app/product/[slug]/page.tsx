188fbdc88b51f38d399f72ba8594a07b7518e05b
Коноплич Николай <kolikamatan@gmail.com>
2026-05-11 13:40:27 +0300

    fix: fix gap

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -200,7 +200,7 @@ export default async function ProductPage({
 				</div>
 
 				{/* Main content: Gallery + Right sidebar */}
-				<div className='flex flex-col lg:flex-row'>
+				<div className='flex flex-col gap-8 lg:flex-row'>
 					{/* Left: Gallery */}
 					<div className='lg:w-[55%] flex flex-col gap-6'>
 						<ProductGallery images={productImages} alt={product.name} />
